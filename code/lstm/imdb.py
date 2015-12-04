1ff6fbe876080b83a8e1005bb42bac52ca74f147
liangpj <378058621@qq.com>
2015-12-04 19:05:59 +0800

    tokenizer

--- code/lstm/imdb.py
+++ code/lstm/imdb.py
@@ -1,1 +1,30 @@
+#-*-coding:utf-8 -*-
 
+"""
+该脚本用于创建 pickled 数据集
+
+1) You need to download this file and put it in the same directory as this file.
+https://github.com/moses-smt/mosesdecoder/raw/master/scripts/tokenizer/tokenizer.perl . Give it execution permission.
+
+2) Get the dataset from http://ai.stanford.edu/~amaas/data/sentiment/ and extract it in the current directory.
+
+3) Then run this script.
+"""
+
+import numpy
+import cPickle as pkl
+
+from collections import OrderedDict
+import glob
+import os
+
+from subprocess import Popen, PIPE
+
+# tokenizer.perl is from Moses: https://github.com/moses-smt/mosesdecoder/tree/master/scripts/tokenizer
+tokenizer_cmd = ['./tokenizer.perl', '-l', 'en', '-q', '-']
+
+
+def tokenize(sentences) :
+
+    print 'Tokenizing ... '
+    text =
